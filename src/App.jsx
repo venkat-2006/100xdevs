a6ea903b1ba737e054d26a1c3895f21ae2790005
VENKAT6912 <vibecoder18@gmail.com>
2025-11-02 01:07:31 +0530

    added main page text and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -59,8 +59,28 @@ function App() {
       </nav>
 
 
-      <div className="bg-black h-screen flex items-center justify-center text-white text-3xl">
-        Hello Tailwind!
+      <div className="bg-black h-screen flex flex-col items-center justify-center text-white text-3xl">
+        <div className="flex flex-col text-7xl font-black tracking-tighter">
+          <div className="flex flex-wrap items-baseline">
+            <span className="bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text text-transparent">
+              100x Devs,
+            </span>
+            <span className="text-white">&nbsp;because</span>
+          </div>
+          <div className="text-white pl-8">10x ain't enough!</div>
+        </div>
+        <div className="p-4 text-lg">
+          A beginner-friendly platform for mastering programming skills.
+        </div>
+        <div className="py-4">
+          <button className="bg-gradient-to-b from-blue-400 to-blue-700 text-white px-6 py-3 rounded-md font-medium hover:opacity-80 transition-all duration-300 text-sm mr-1">
+            Explore courses
+          </button>
+          <button className="bg-white text-black px-6 py-3 rounded-md text-sm hover:bg-gray-100 transition-colors duration-200 ml-1">
+            Explore Notes
+          </button>
+        </div>
+
       </div>
     </>
   );
