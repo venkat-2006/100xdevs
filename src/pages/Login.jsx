0459b1d4680db0e866f20e6684fd8f0eb2afbf95
VENKAT6912 <vibecoder18@gmail.com>
2025-11-17 21:26:12 +0530

    small placing fixes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,26 +10,26 @@ export default function Login({ lightMode, setLightMode }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    navigate("/dashboard"); 
+    navigate("/dashboard");
   }
 
   return (
     <>
       <Navbar lightMode={lightMode} setLightMode={setLightMode} />
-      <div className={`min-h-screen flex items-center justify-center px-4 transition-colors duration-300 ${
-        lightMode 
-          ? 'bg-gradient-to-b from-white to-gray-100' 
-          : 'bg-gradient-to-b from-black to-[#0a0a0a]'
-      }`}>
-        <div className="w-[420px] h-[420px] bg-white/6 dark:bg-white/95 rounded-xl shadow-xl backdrop-blur-md border border-white/10 dark:border-gray-200/20 p-8 flex flex-col justify-between">
-          <div>
-            <h1 className="text-2xl font-extrabold text-white dark:text-black">
+      <div className={`min-h-screen flex items-center justify-center px-4 transition-colors duration-300 ${lightMode
+          ? 'bg-gradient-to-b from-white to-gray-100'
+          : 'bg-[linear-gradient(to_bottom,#000000_0%,#1a1a1a_30%,#0a0a0a_70%,#000000_100%)]'
+        }`}>
+        <div className="w-[420px] h-[420px]  bg-white/6 dark:bg-white/95 rounded-xl shadow-xl backdrop-blur-md border border-white/10 dark:border-gray-200/20 p-8 flex flex-col justify-between">
+          <div className="text-center">
+            <h1 className="text-[2.1rem] font-extrabold text-white dark:text-black">
               Welcome to <span className="bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text text-transparent">100xDevs</span>
             </h1>
-            <p className="mt-2 text-sm text-gray-300 dark:text-gray-700">
+            <p className="mt-0 text-xl text-gray-300 dark:text-gray-700">
               Log in to access paid content!
             </p>
           </div>
+
 
           <form className="mt-6 flex-1 flex flex-col justify-center" onSubmit={handleSubmit}>
             <label className="text-xs text-gray-300 dark:text-gray-700 mb-2">Email</label>
@@ -39,7 +39,7 @@ export default function Login({ lightMode, setLightMode }) {
               onChange={(e) => setEmail(e.target.value)}
               placeholder="name@email.com"
               required
-              className="w-full px-4 py-2 rounded-md bg-white/5 border border-white/10 dark:bg-white dark:border-gray-200 text-white dark:text-black placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400 transition mb-4"
+              className="w-full px-4 py-2 rounded-md bg-white/5 border border-white/10 dark:bg-white dark:border-gray-200 text-white dark:text-black placeholder:text-gray-400 dark:placeholder:text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-400 transition mb-4"
             />
 
             <label className="text-xs text-gray-300 dark:text-gray-700 mb-2">Password</label>
@@ -99,7 +99,7 @@ export default function Login({ lightMode, setLightMode }) {
             </button>
           </form>
 
-          
+
         </div>
       </div>
     </>
